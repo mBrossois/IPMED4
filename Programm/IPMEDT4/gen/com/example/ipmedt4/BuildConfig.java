2d93324439feb74b8c7d9705afcf2107b5d1cb3c
mBrossois <mark.dnb@live.nl>
2014-11-19 15:52:41 +0100

    p

--- Programm/IPMEDT4/gen/com/example/ipmedt4/BuildConfig.java
+++ Programm/IPMEDT4/gen/com/example/ipmedt4/BuildConfig.java
@@ -1,5 +1,5 @@
 /** Automatically generated file. DO NOT MODIFY */
-package com.example.troep;
+package com.example.ipmedt4;
 
 public final class BuildConfig {
     public final static boolean DEBUG = true;
--- Programm/IPMEDT4/src/com/example/ipmedt4/MainActivity.java
+++ Programm/IPMEDT4/src/com/example/ipmedt4/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.troep;
+package com.example.ipmedt4;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
