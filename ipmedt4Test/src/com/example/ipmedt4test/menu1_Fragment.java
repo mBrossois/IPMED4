143146cbe1e1dafcb4b1570759f6fe7d866b769b
giovski <s1084864@student.hsleiden.nl>
2015-01-07 14:41:37 +0100

    Eerste app met studio
    Menu balk is werkend en schermen kunnen gewiseld worden

---
+++ ipmedt4Test/src/com/example/ipmedt4test/menu1_Fragment.java
@@ -0,0 +1,18 @@
+package com.example.ipmedt4test;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class menu1_Fragment extends Fragment {
+	View rootview;
+	@Nullable
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+		rootview = inflater.inflate(R.layout.menu1_layout,container,false);
+		return rootview;
+	}
+}
